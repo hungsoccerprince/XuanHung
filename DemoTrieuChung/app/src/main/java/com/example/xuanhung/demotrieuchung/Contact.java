9f346a7076e917a155f6b7b1bda7a49a94c92727
hungsoccerprince <hungsoccerprince@gmail.com>
2016-04-25 16:23:34 +0700

    demo trieu chung

---
+++ DemoTrieuChung/app/src/main/java/com/example/xuanhung/demotrieuchung/Contact.java
@@ -0,0 +1,36 @@
+package com.example.xuanhung.demotrieuchung;
+
+import java.util.ArrayList;
+
+/**
+ * Created by MaiNam on 4/15/2016.
+ */
+public class Contact {
+    private String mName;
+    private boolean mOnline;
+
+    public Contact(String name, boolean online) {
+        mName = name;
+        mOnline = online;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public boolean isOnline() {
+        return mOnline;
+    }
+
+    private static int lastContactId = 0;
+
+    public static ArrayList<Contact> createContactsList(int numContacts) {
+        ArrayList<Contact> contacts = new ArrayList<Contact>();
+
+        for (int i = 1; i <= numContacts; i++) {
+            contacts.add(new Contact("Person " + ++lastContactId, i <= numContacts / 2));
+        }
+
+        return contacts;
+    }
+}
